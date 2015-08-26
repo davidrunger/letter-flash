352e02fd945e0a2493cd35779c9e420165c815c1
David Runger <daverunger@gmail.com>
2015-08-25 19:22:09 -0700

    initial commit after extracting from other repo
    This project was in my davidrunger.github.io directory before I
    remembered/realized that I can maintain it as a separate project.

---
+++ javascipts/flash.js
@@ -0,0 +1,144 @@
+;(function() {
+  var Flash = window.Flash = window.Flash || {};
+  Flash.INTERVAL = 10;
+  Flash.CPS = 1.8;
+  Flash.letters = JSON.parse(window.localStorage.getItem('letters')) || ['f', 'j'];
+  Flash.timeouts = [];
+
+  Flash.initialize = function () {
+    Flash.$progressBar = $('.progress-bar');
+    selectKeys();
+    setup();
+    bindEvents();
+  };
+
+  function bindEvents() {
+    $('body').on('keypress', handleKeypress.bind());
+    $('.key').click(function (event) {
+      var $el = $(event.currentTarget);
+      $el.toggleClass('selected');
+
+      var letter = $el.text();
+      toggleLetter(letter);
+    });
+  }
+
+  function changeLetter() {
+    var oldLetter = Flash.currentLetter;
+    while (Flash.currentLetter === oldLetter) {
+      Flash.currentLetter = _.sample(Flash.letters);
+    }
+    $('.character').text(Flash.currentLetter);
+  }
+
+  function changeRight(diff, lettersInPlay) {
+    if (diff === -1 && lettersInPlay !== Flash.letters.length) { return; }
+
+    Flash.right += diff;
+    $('.right').text(Flash.right);
+  }
+
+  function changeWrong(diff, lettersInPlay) {
+    Flash.wrong += diff;
+    $('.wrong').text(Flash.wrong);
+  }
+
+  function clearTimeouts() {
+    var timeouts = Flash.timeouts;
+    for (var i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+    }
+  }
+
+  function handleKeypress(event) {
+    var allowedPunctuation = [44, 46, 59].indexOf(event.which) !== -1;
+    if ((event.which < 65 &&!allowedPunctuation) || event.which > 122) {
+      return;
+    }
+
+    var typedChar = String.fromCharCode(event.which).toLowerCase();
+    if (typedChar === Flash.currentLetter) {
+      markRight();
+      changeLetter();
+    } else {
+      markWrong();
+    }
+
+    updateCPM();
+  }
+
+  function levelUp() {
+    alert("You've reached 1.8 chars/sec. Maybe add a letter?");
+    setup();
+  }
+
+  function markRight() {
+    var lettersInPlay = Flash.letters.length;
+    changeRight(1);
+    var timeout = setTimeout(function () {
+      changeRight(-1, lettersInPlay);
+      updateCPM();
+    }, 1000 * Flash.INTERVAL);
+    Flash.timeouts.push(timeout);
+  }
+
+  function markWrong() {
+    changeWrong(1);
+    var timeout = setTimeout(function () {
+      changeWrong(-1, Flash.letters.length);
+      updateCPM();
+    }, 1000 * Flash.INTERVAL);
+    Flash.timeouts.push(timeout);
+  }
+
+  function score() {
+    return ( Flash.right - ( 4 * Flash.wrong ) ) / Flash.INTERVAL;
+  }
+
+  function selectKeys() {
+    Flash.letters.forEach(function (letter) {
+      var $key = $('.key:contains(' + letter + ')');
+      $key.addClass('selected');
+    });
+  }
+
+  function setup() {
+    clearTimeouts();
+    changeLetter();
+    $('.character').text(Flash.currentLetter);
+
+    Flash.right = 0;
+    Flash.wrong = 0;
+
+    $('.right').text('0');
+    $('.wrong').text('0');
+    updateCPM();
+  }
+
+  function toggleLetter(letter) {
+    var idx = Flash.letters.indexOf(letter);
+    if (idx === -1) {
+      Flash.letters.push(letter);
+    }
+    else {
+      Flash.letters.splice(idx, 1);
+    }
+    window.localStorage.setItem('letters', JSON.stringify(Flash.letters));
+    changeLetter();
+  }
+
+  function updateCPM() {
+    $('.cps').text( score() );
+    updateProgressBar();
+    if ( score() >= Flash.CPS) {
+      levelUp();
+    }
+  }
+
+  function updateProgressBar() {
+    var percentOfCpsGoal = score() / Flash.CPS;
+    var percentNotAccomplished = 1 - percentOfCpsGoal;
+    var pixelsNotAccomplished = Math.max(0, 390 * percentNotAccomplished);
+    Flash.$progressBar.css('top', pixelsNotAccomplished);
+  }
+})();
---
+++ todo.md
@@ -0,0 +1,8 @@
+# letter-flash To-Do's
+
+* allow customization of chars/sec target
+* allow customization of progress bar color
+* use modal or other more elegant popup instead of JS alert
+* add some fancy styling
+* prevent registering keypress when using modifier keys
+* don't have negative points for errors; just restart them with 0 correct
